882bd2e9c1e0ef8286717ba9d4173fe80a0b5003
vishnu <vishnu.dsvv@gmail.com>
2014-07-02 22:05:41 +0530

    first commit

---
+++ links/views.py
@@ -0,0 +1,38 @@
+from django.views.generic import ListView
+from .models import Link, Vote
+from django.views.generic import DetailView
+from django.contrib.auth import get_user_model
+from .models import UserProfile
+from django.views.generic.edit import UpdateView
+from .models import UserProfile
+from .forms import UserProfileForm
+from django.core.urlresolvers import reverse
+
+
+class LinkListView(ListView):
+    model = Link
+    queryset = Link.with_votes.all()
+    paginate_by = 3
+
+
+class UserProfileDetailView(DetailView):
+    model = get_user_model()
+    slug_field = "username"
+    template_name = "user_detail.html"
+
+    def get_object(self, queryset=None):
+        user = super(UserProfileDetailView, self).get_object(queryset)
+        UserProfile.objects.get_or_create(user=user)
+        return user
+
+
+class UserProfileEditView(UpdateView):
+    model = UserProfile
+    form_class = UserProfileForm
+    template_name = "edit_profile.html"
+
+    def get_object(self, queryset=None):
+        return UserProfile.objects.get_or_create(user=self.request.user)[0]
+
+    def get_success_url(self):
+        return reverse("profile", kwargs={'slug': self.request.user})
